92e5086bb75c9f616378e50655516063bc1e24e0
RoyCoates <Coates-R4@ulster.ac.uk>
2025-04-13 11:53:42 +0100

    Create EGM722Project.py
    Packages required

---
+++ EGM722Project.py
@@ -0,0 +1,9 @@
+# Imports the required packages
+import os
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from cartopy.feature import ShapelyFeature
+import cartopy.crs as ccrs
+import matplotlib.patches as mpatches
+import matplotlib.lines as mline
+import folium
